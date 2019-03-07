74fadef618fc4bcd6324d45edb0661a471659e28
tomu28 <b1700662@planet.kanazawa-it.ac.jp>
2019-03-07 23:22:58 +0900

    Rxを利用してUIのイベントから処理を行う例を追加

--- Learning_RxSwift/Learning_RxSwift/ViewController.swift
+++ Learning_RxSwift/Learning_RxSwift/ViewController.swift
@@ -4,7 +4,7 @@
 //
 //  Created by 小幡 十矛 on 2019/03/05.
 //  Copyright © 2019 Tomu Obata. All rights reserved.
-//  reference https://codezine.jp/article/detail/11051?p=2
+//  reference https://codezine.jp/article/detail/11051?p=3
 
 import UIKit
 import RxSwift
@@ -12,20 +12,27 @@ import RxCocoa
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sampleButton: UIButton!
+    @IBOutlet weak var sampleSwitch: UISwitch!
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        // Observableを生成
-        let helloObservable: Observable<String> = Observable.from(["H","E","L","L","O"])
-        // メソッドチェーンver
-        helloObservable.subscribe(
-            onNext: { print($0) },
-            onError: { print($0.localizedDescription) },
-            onCompleted: { print("completed") }
-            ).disposed(by: DisposeBag())
-    }
+        // Rxを利用してUIのイベントから処理を行う例
+        sampleButton.rx.tap.subscribe(
+            onNext: {
+                print("tap")
+            }
+        ).disposed(by: disposeBag)
+    
+        sampleSwitch.rx.isOn.subscribe(
+            onNext: { bool in
+                print( bool ? " ON " : " OFF " )
+            }
+        ).disposed(by: disposeBag)
 
+        }
 
 }
-
